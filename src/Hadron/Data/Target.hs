67e77ba6f77b4c65c1cc8e360244609c8de64534
Sharif Olorin <sio@tesser.org>
2016-06-24 08:03:18 +0000

    Request target types

--- src/Hadron/Data/Target.hs
+++ src/Hadron/Data/Target.hs
@@ -3,9 +3,18 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Hadron.Data.Target(
     RequestTarget(..)
+  , QueryString(..)
+  , URIPath(..)
+
+  , renderQueryString
+  , renderRequestTarget
+  , renderURIPath
   ) where
 
 import           Control.DeepSeq.Generics (genericRnf)
+
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
 
 import           GHC.Generics (Generic)
 
@@ -20,7 +29,38 @@ import           P
 --
 -- FIXME: implement the others (asterisk, absolute, target).
 data RequestTarget =
-    AbsPathTarget
+    AbsPathTarget !URIPath !QueryString
   deriving (Eq, Show, Generic)
 
 instance NFData RequestTarget where rnf = genericRnf
+
+renderRequestTarget :: RequestTarget -> ByteString
+renderRequestTarget (AbsPathTarget p qs) = BS.concat [
+    renderURIPath p
+  , renderQueryString qs
+  ]
+
+-- | The part between the host part and the query string in a URL. We don't
+-- care about manipulating 'URIPath's at all, so we don't need any more
+-- structure than a ByteString newtype for validation.
+newtype URIPath =
+  URIPath {
+    unURIPath :: ByteString
+  } deriving (Eq, Show, Generic)
+
+instance NFData URIPath where rnf = genericRnf
+
+renderURIPath :: URIPath -> ByteString
+renderURIPath = unURIPath
+
+-- | The part after the '?', if there is a '?'.
+data QueryString =
+    NoQueryString
+  | QueryStringPart !ByteString
+  deriving (Eq, Show, Generic)
+
+instance NFData QueryString where rnf = genericRnf
+
+renderQueryString :: QueryString -> ByteString
+renderQueryString NoQueryString = ""
+renderQueryString (QueryStringPart qs) = "?" <> qs
