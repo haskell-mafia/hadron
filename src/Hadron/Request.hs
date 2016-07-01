c01f15892de47e04fe8c5ce05546846b42627d16
Sharif Olorin <sio@tesser.org>
2016-07-01 04:41:50 +0000

    Add headers to request objects

--- src/Hadron/Request.hs
+++ src/Hadron/Request.hs
@@ -2,20 +2,29 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Hadron.Request(
     parseHTTPRequest
+  , addRequestHeader
   ) where
 
 import qualified Data.Attoparsec.ByteString as AB
 import           Data.ByteString (ByteString)
+import           Data.Semigroup ((<>))
 import qualified Data.Text as T
 
-import           Hadron.Data.Request
+import           Hadron.Data
 import           Hadron.Error
 import           Hadron.Parser.Request
 
-import           P
+import           P hiding ((<>))
 
 -- | Take a ByteString representing one complete HTTP request and provide
 -- either a parsed version or an error.
 parseHTTPRequest :: ByteString -> Either RequestError HTTPRequest
 parseHTTPRequest =
   first (RequestParsingError . T.pack) . AB.parseOnly httpRequestP
+
+addRequestHeader :: HTTPRequest -> Header -> HTTPRequest
+addRequestHeader (HTTPV1_1Request req) newH =
+  let oldHs = unHTTPRequestHeaders $ hrqv1_1Headers req
+      newHs = HTTPRequestHeaders $ (pure newH) <> oldHs in
+  HTTPV1_1Request $ req { hrqv1_1Headers = newHs }
+
