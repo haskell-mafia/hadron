7b216eb5e705f3e7d6687de89629617da0a569fe
Sharif Olorin <sio@tesser.org>
2016-07-08 07:50:22 +0000

    Add removeRequestHeader

--- src/Hadron/Request.hs
+++ src/Hadron/Request.hs
@@ -4,6 +4,7 @@ module Hadron.Request(
     parseHTTPRequest
   , addRequestHeader
   , lookupRequestHeader
+  , removeRequestHeader
   ) where
 
 import qualified Data.Attoparsec.ByteString as AB
@@ -25,11 +26,30 @@ parseHTTPRequest :: ByteString -> Either RequestError HTTPRequest
 parseHTTPRequest =
   first (RequestParsingError . T.pack) . AB.parseOnly httpRequestP
 
+-- | Return a request with a header added. This function does not do
+-- any canonicalization; if the request already has a header with the
+-- the provided name, the resulting request will have two Header
+-- values with that HeaderName.
 addRequestHeader :: HTTPRequest -> Header -> HTTPRequest
 addRequestHeader (HTTPV1_1Request req) newH =
   let oldHs = unHTTPRequestHeaders $ hrqv1_1Headers req
       newHs = HTTPRequestHeaders $ (pure newH) <> oldHs in
   HTTPV1_1Request $ req { hrqv1_1Headers = newHs }
+
+-- | Remove a header from a request by name. Returns Nothing' if this
+-- would render the request invalid. Returns the request unchanged if
+-- the header is not present.
+removeRequestHeader :: HTTPRequest -> HeaderName -> Maybe' HTTPRequest
+removeRequestHeader (HTTPV1_1Request req) hn
+  | hn == HeaderName "host" = Nothing'
+  | otherwise =
+    let oldHs = unHTTPRequestHeaders $ hrqv1_1Headers req
+        filteredHs = filter ((/= hn) . httpHeaderName) $ NE.toList oldHs in
+    case nonEmpty filteredHs of
+      Nothing ->
+        Nothing'
+      Just newHs ->
+        pure . HTTPV1_1Request $ req { hrqv1_1Headers = HTTPRequestHeaders newHs }
 
 lookupRequestHeader :: HTTPRequest -> HeaderName -> Maybe' (NonEmpty HeaderValue)
 lookupRequestHeader (HTTPV1_1Request req) hn =
